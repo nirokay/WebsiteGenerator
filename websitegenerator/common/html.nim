3bf75524c7fd4ea506cd26bff54b045c96e0ca98
nirokay <80123634+nirokay@users.noreply.github.com>
2023-08-14 17:37:30 +0200

    removed class passing for common htmls

--- websitegenerator/common/html.nim
+++ websitegenerator/common/html.nim
@@ -11,30 +11,29 @@ proc header(content: string, number: int): HtmlElement = newElement(
     "h" & $number, content
 )
 
-proc h1*(text: string, class: string = ""): HtmlElement = header(text, 1) ## Highest header
-proc h2*(text: string, class: string = ""): HtmlElement = header(text, 2) ## Second header
+proc h1*(text: string): HtmlElement = header(text, 1) ## Highest header
+proc h2*(text: string): HtmlElement = header(text, 2) ## Second header
 
-proc p*(text: string, class: string = ""): HtmlElement = newElement("p", text) ## Paragraph
+proc p*(text: string): HtmlElement = newElement("p", text) ## Paragraph
 
-proc `div`*(elements: seq[HtmlElement], class: string = ""): HtmlElement = newElement("div", $elements)
+proc `div`*(elements: seq[HtmlElement]): HtmlElement = newElement("div", $elements)
 
-proc `div`*(class: string = "", elements: varargs[HtmlElement]): HtmlElement =
+proc `div`*(elements: varargs[HtmlElement]): HtmlElement =
     var temp: seq[HtmlElement]
     for element in elements:
         temp.add(element)
     newElement("div", $temp)
 
-proc b*(text: string, class: string = ""): HtmlElement = newElement("b", text) ## Bold / "Bring attention to"
+proc b*(text: string): HtmlElement = newElement("b", text) ## Bold / "Bring attention to"
 
-proc text*(text: string, class: string = ""): HtmlElement = newElement("", text) ## Raw text
+proc a*(href, content: string): HtmlElement = newElement("a", @[over("href", href)], content)
 
-proc link*(href: string, class: string = ""): HtmlElement = newElement("a", @[
-    over("href", href)
-])
-proc link*(rel, href: string, class: string = ""): HtmlElement = newElement("link", @[
+proc text*(text: string): HtmlElement = newElement("", text) ## Raw text
+
+proc link*(rel, href: string): HtmlElement = newElement("link", @[
     over("rel", rel), over("href", href)
 ])
-proc link*(rel, `type`, href: string, class: string = ""): HtmlElement = newElement("link", @[
+proc link*(rel, `type`, href: string): HtmlElement = newElement("link", @[
     over("rel", rel), over("type", `type`), over("href", href)
 ])
 
