6660f4647143da19fd6a8740dc6d5f1dcb268f79
nirokay <80123634+nirokay@users.noreply.github.com>
2023-08-17 13:13:13 +0200

    added pain example

---
+++ examples/pain/index.nim
@@ -0,0 +1,38 @@
+import std/[strutils]
+import websitegenerator
+
+var
+    html: HtmlDocument = newDocument("index.html")
+    css: CssStyleSheet = newCssStyleSheet("styles.css")
+
+let
+    body: CssElement = newCssElement("body",
+        ["color", "rgb(0, 255, 0)"],
+        ["background-color", "magenta"]
+    )
+    painful: CssElement = newCssClass("painful",
+        ["color", "lime"]
+    )
+
+css.add(
+    body,
+    painful
+)
+
+html.setStyle(css)
+
+html.addToBody(
+    h1("PAIN AND SUFFERING"),
+    p("I hope you did not open this page in a dark room..."),
+
+    dialog(@[
+        h1($b("Dialog box")),
+        p("very cool! You can only see half of the page now :)")
+    ]).setClass(painful),
+
+    p("This is some text that is hidden behind the dialog box!"),
+
+    small(repeat($br(), 50) & "this is horrible...")
+)
+css.writeFile()
+html.writeFile()
