6f188fbe23305dd1e65d4d433bab85e5471654fd
nirokay <nirokay-public@protonmail.com>
2024-09-24 09:22:35 +0200

    added class appending procs

--- websitegenerator/sugar.nim
+++ websitegenerator/sugar.nim
@@ -12,6 +12,7 @@ proc `[]`*(tag: string, tagAttributes: varargs[HtmlElementAttribute]): HtmlEleme
     newHtmlElement(tag, tagAttributes)
 
 proc `=>`*(attribute, value: string): HtmlElementAttribute =
+    ## Sugar constructor for `HtmlElementAttribute`
     result = attr(attribute, value)
 
 proc `{}`*(element: string, properties: varargs[CssAttribute]): CssElement =
@@ -42,33 +43,74 @@ proc `&=>`*(element: HtmlElement, content: string|HtmlElement): HtmlElement =
 proc `->`*(element: var HtmlElement, class: string) =
     ## Sugar for class assignment
     element.class = class
-proc `->`*(element: var HtmlElement, class: CssElement) =
-    ## Sugar for class assignment, raises `ValueError` when passed `CssElement` is not a class
-    if not class.isClass:
-        raise ValueError.newException("Class assignment: CssElement is not a class")
-    element -> class.name
-
 proc `->`*(element: HtmlElement, class: string): HtmlElement =
     ## Sugar for class assignment
     result = element
     result -> class
+
+proc `->`*(element: var HtmlElement, class: CssElement) =
+    ## Sugar for class assignment, raises `ValueError` when passed `CssElement` is not a class
+    if not class.isClass:
+        raise ValueError.newException("Class assignment: CssElement '" & class.name & "' is not a class")
+    element -> class.name
 proc `->`*(element: HtmlElement, class: CssElement): HtmlElement =
     ## Sugar for class assignment, raises `ValueError` when passed `CssElement` is not a class
     result = element
     result -> class
 
+proc `&->`*(element: var HtmlElement, class: string) =
+    ## Sugar for appending a new class
+    element -> strip(element.class & " " & class)
+proc `&->`*(element: HtmlElement, class: string): HtmlElement =
+    ## Sugar for appending a new class
+    result = element
+    result &-> class
+
+proc `&->`*(element: var HtmlElement, class: CssElement) =
+    ## Sugar for appending a new class, raises `ValueError` when passed `CssElement` is not a class
+    if not class.isClass:
+        raise ValueError.newException("Class appending: CssElement '" & class.name & "' is not a class")
+    element &-> class.name
+proc `&->`*(element: HtmlElement, class: CssElement): HtmlElement =
+    ## Sugar for appending a new class, raises `ValueError` when passed `CssElement` is not a class
+    result = element
+    result &-> class
+
+proc `&->`*(element: var HtmlElement, classes: seq[string]) =
+    ## Sugar for appending a new class
+    for class in classes:
+        element &-> class
+proc `&->`*(element: HtmlElement, classes: seq[string]): HtmlElement =
+    ## Sugar for appending a new class
+    result = element
+    result &-> classes
+
+proc `&->`*(element: var HtmlElement, classes: seq[CssElement]) =
+    ## Sugar for appending a new class, raises `ValueError` when passed `CssElement` is not a class
+    for class in classes:
+        element &-> class.name
+proc `&->`*(element: HtmlElement, classes: seq[CssElement]): HtmlElement =
+    ## Sugar for appending a new class, raises `ValueError` when passed `CssElement` is not a class
+    result = element
+    result &-> classes
+
+
 proc addStyle*(element: var HtmlElement, attributes: seq[CssAttribute]) =
+    ## Adds a/appends to the `style` HTML attribute
     var style: string
     for attribute in attributes:
         style.add attribute[0] & ":" & attribute[1] & ";"
     element.addattr("style", style)
 proc addStyle*(element: var HtmlElement, attributes: varargs[CssAttribute]) =
+    ## Adds a/appends to the `style` HTML attribute
     element.addStyle(attributes.toSeq())
 
 proc addStyle*(element: HtmlElement, attributes: seq[CssAttribute]): HtmlElement =
+    ## Adds a/appends to the `style` HTML attribute
     result = element
     result.addStyle(attributes)
 proc addStyle*(element: HtmlElement, attributes: varargs[CssAttribute]): HtmlElement =
+    ## Adds a/appends to the `style` HTML attribute
     result = element
     result.addStyle(attributes.toSeq())
 
@@ -106,7 +148,7 @@ runnableExamples:
     # Class assignment:
     header -> classCenter
 
-    header = header.attrStyle(
+    header.addStyle(
         "text-align" := "left" # Overriding for a single element
     )
 
